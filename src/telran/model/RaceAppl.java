08a81ed2b8ed816fc2a69c62a7e050a54c42c22d
adeli <adelinapribega@gmail.com>
2025-04-27 23:19:53 +0300

    Second try

--- src/telran/model/RaceAppl.java
+++ src/telran/model/RaceAppl.java
@@ -1,15 +1,21 @@
 package telran.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class RaceAppl {
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Wrong number of arguments");
-            return;
-        }
-        try {
-            Tarakan[] tasks = new Tarakan[Integer.parseInt(args[0])];
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Write number of tarakans for the race:");
+        int nTar = Integer.parseInt(br.readLine());
+        System.out.println("Write distance of the race:");
+        int dist = Integer.parseInt(br.readLine());
+
+        Tarakan[] tasks = new Tarakan[nTar];
             for (int i = 0; i < tasks.length; i++) {
-                tasks[i] = new Tarakan("tarakan #" + (i + 1), Integer.parseInt(args[0]));
+                tasks[i] = new Tarakan("tarakan #" + (i + 1), dist);
             }
             Thread[] threads = new Thread[tasks.length];
             for (int i = 0; i < threads.length; i++) {
@@ -22,11 +28,6 @@ public class RaceAppl {
                 threads[i].join();
             }
 
-        }catch (NumberFormatException e) {
-            System.out.println("Wrong number format");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
 
     }
